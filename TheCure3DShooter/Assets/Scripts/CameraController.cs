b5d5613171bef3348951e63bca3d79ed0f1c500f
Robin Bono <robin@omnick.se>
2020-10-26 14:52:24 +0100

    Added basic camera

---
+++ TheCure3DShooter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+
+    GameObject player;
+
+    Vector3 pathFrom;
+    Vector3 pathGoal;
+
+    float moveSpeed = 10;
+    float lerpStep = 0;
+
+    void Start() {
+
+        player = GameObject.FindGameObjectWithTag (Tags.player);
+    }
+
+    void Update() {
+
+        if( lerpStep < 1 ) {
+            
+            lerpStep += moveSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp (pathFrom, pathGoal, lerpStep);
+        }
+    }
+
+    public void SetPathGoal( Vector3 goal ) {
+
+        pathFrom = transform.position;
+        pathGoal = goal;
+        lerpStep = 0;
+    }
+}
