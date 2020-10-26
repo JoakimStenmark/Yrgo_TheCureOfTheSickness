0c958957e03fa95d6d7ac0fa762fa32a6caee44b
Robin Bono <robin@omnick.se>
2020-10-26 15:44:41 +0100

    Basic map generation

---
+++ TheCure3DShooter/Assets/Scripts/TunnelController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelController : MonoBehaviour
+{
+
+    public GameObject[] tunnelSegments;
+
+    int mapLength = 50;
+    public float offset;
+
+    void Start() {
+        
+        GenerateMap();
+    }
+
+    void Update() {
+        
+        
+    }
+
+    void GenerateMap() {
+
+        Vector3 lastPosition;
+        lastPosition = Vector3.zero;
+
+        for( int i = 1; i < mapLength; i++ ) {
+
+            Vector3 newPosition = new Vector3(
+
+                Random.Range(lastPosition.x - offset, lastPosition.x + offset),
+                Random.Range(lastPosition.y - offset, lastPosition.y + offset),
+                i
+            );
+
+            Instantiate (tunnelSegments[0], newPosition, Quaternion.identity);
+        }
+    }
+}
