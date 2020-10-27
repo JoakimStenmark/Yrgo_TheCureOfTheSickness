d729cac182725a496e062312f446ad6cf6eda2f9
Robert Sandh <sarobban@gmail.com>
2020-10-27 13:44:25 +0100

    Update PathManager.cs

--- TheCure3DShooter/Assets/Scripts/LevelManager/PathManager.cs
+++ TheCure3DShooter/Assets/Scripts/LevelManager/PathManager.cs
@@ -72,7 +72,7 @@ public class PathManager : MonoBehaviour {
 
             pathGoal = new Vector3(xyVector.x, xyVector.y, pathGoal.z);
 
-            returnValue = pathGoal;// Vector3.MoveTowards(currentPosition, pathGoal, speed * Time.deltaTime);
+            returnValue = Vector3.MoveTowards(currentPosition, pathGoal, speed * Time.deltaTime);
         }
 
         return returnValue;
