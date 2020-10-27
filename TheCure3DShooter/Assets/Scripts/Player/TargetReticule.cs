cf281ee4a676b228e089546b04e254534e5e6fef
Joakim Stenmark <jocke.stenmark@live.se>
2020-10-27 17:10:10 +0100

    Corrected target distance

--- TheCure3DShooter/Assets/Scripts/Player/TargetReticule.cs
+++ TheCure3DShooter/Assets/Scripts/Player/TargetReticule.cs
@@ -7,20 +7,25 @@ public class TargetReticule : MonoBehaviour
 {
 
     public float distanceFromPlayer;
-    public float distanceBetweenPlayerAndCamera;
+    private float distanceBetweenPlayerAndCamera;
+    private GameObject player;
 
     void Start()
     {
-        
+        player = transform.parent.gameObject;
     }
 
     void LateUpdate()
     {
-        distanceBetweenPlayerAndCamera = GetComponentInParent<Transform>().position.z - Camera.main.transform.position.z;
+        float playerZPosition = transform.parent.gameObject.transform.position.z;
+        float cameraZPosition = Camera.main.transform.position.z;
 
-        Debug.Log(GetComponentInParent<Transform>().position.z);
+        distanceBetweenPlayerAndCamera = playerZPosition - cameraZPosition;
 
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceFromPlayer);
+        Debug.Log(playerZPosition);
+
+
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceBetweenPlayerAndCamera + distanceFromPlayer);
         
         transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
            
