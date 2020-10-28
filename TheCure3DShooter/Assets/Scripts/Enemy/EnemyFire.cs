de63e95bbdafaffe1ec824eda1505a876453d455
Robert Sandh <sarobban@gmail.com>
2020-10-28 14:58:17 +0100

    EnemyFire

---
+++ TheCure3DShooter/Assets/Scripts/Enemy/EnemyFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    public Transform target;
+    public float fireRate = 2;
+    private float nextShoot;
+
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        nextShoot = Random.Range(0, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextShoot -= Time.deltaTime;
+        if(nextShoot < 0)
+        {
+            Vector3 dir = target.position + Vector3.forward * 5 - transform.position;
+            Instantiate(projectile, transform.position, Quaternion.LookRotation(dir));
+            nextShoot = fireRate;
+        }
+    }
+}
