e951b057b44c9a9f3a66bcde20b2cc9dc2be8ba4
Robin Bono <robin@omnick.se>
2020-10-28 13:08:43 +0100

    Menu update

---
+++ TheCure3DShooter/Assets/Scripts/Menu/FlyBy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyBy : MonoBehaviour {
+
+    public Vector3 spinSpeed;
+    public float resetDistance;
+    public Vector3 resetDestination;
+    public float resetDestinationOffset;
+
+    void Start() {
+
+        ResetPosition();
+    }
+
+    void Update() {
+
+
+        transform.Rotate(spinSpeed);
+
+        if( transform.position.z > resetDistance ) {
+
+            ResetPosition();
+        }
+    }
+
+    void ResetPosition() {
+
+        transform.position = new Vector3(
+
+            Random.Range(resetDestination.x - resetDestinationOffset, resetDestination.x + resetDestinationOffset),
+            Random.Range(resetDestination.y - resetDestinationOffset, resetDestination.y + resetDestinationOffset),
+            Random.Range(resetDestination.z - resetDestinationOffset, resetDestination.z + resetDestinationOffset)
+        );
+    }
+}
