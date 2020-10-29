279c6d713d2835485651c9c9d28b2816111f3d73
Joakim Stenmark <jocke.stenmark@live.se>
2020-10-29 17:42:45 +0100

    Death sound for bacteria

---
+++ TheCure3DShooter/Assets/Scripts/SoundEffectPlaylist.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectPlaylist : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    private AudioSource soundPlayer;
+    void Start()
+    {
+        soundPlayer = GetComponent<AudioSource>();
+
+        int clipNumber = Random.Range(0, audioClips.Length);
+        soundPlayer.clip = audioClips[clipNumber];
+        soundPlayer.pitch = Random.Range(0.8f, 1.1f);
+        soundPlayer.Play();
+        Debug.Log("Play?");
+
+    }
+        
+
+}
