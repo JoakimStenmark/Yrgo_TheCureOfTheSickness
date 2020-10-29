a1118e5cf5521fb146282868ac962befa361039c
Robert Sandh <sarobban@gmail.com>
2020-10-29 22:32:51 +0100

    Bacteria upt.

---
+++ TheCure3DShooter/Assets/Scripts/Enemy/EnemyRandomRotation.cs
@@ -0,0 +1,20 @@
+﻿//Robert S
+//Ful fix
+using UnityEngine;
+
+public class EnemyRandomRotation : MonoBehaviour
+{
+    public float speed = 100;
+    private Vector3 rotAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotAxis = Random.insideUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotAxis, speed * Time.deltaTime);
+    }
+}
